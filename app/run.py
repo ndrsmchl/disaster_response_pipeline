0a5de93fb152e665933c2c4f6fc3d2e96022ab4f
Andreas Quandt <quandtreas@mailbox.org>
2020-08-10 17:29:45 +0200

    Added import of custom feature class and update file paths.

--- app/run.py
+++ app/run.py
@@ -8,9 +8,10 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
+from models.train_classifier import UpperCaseWordsRatioExtractor
 
 app = Flask(__name__)
 
@@ -26,11 +27,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///data/DisasterResponse.db')
+df = pd.read_sql_table('messages_categorized', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
